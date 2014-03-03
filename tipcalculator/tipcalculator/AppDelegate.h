a893fea9fe1fb763c81cba035ce74f45dd42d2dd
Yoe Joon Lee <yoejoon@yahoo-inc.com>
2014-03-03 08:57:09 -0800

    adding tip calculator

---
+++ tipcalculator/tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Yoejoon Lee on 2/28/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\yoejoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Yoejoon Lee on 2/28/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\yoejoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
